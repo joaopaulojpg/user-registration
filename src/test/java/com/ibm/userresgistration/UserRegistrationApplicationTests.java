fbff99b3f71b382cc381154eb159c99c9a96e593
r3n4nm-dev <r3n4nm@gmail.com>
2021-08-18 13:12:10 -0300

    Project created

---
+++ src/test/java/com/ibm/userresgistration/UserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.userresgistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
